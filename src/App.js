2ef250fbd6a9735b8f6cd48515af20ff11cf5fa1
Ijodbek Mehmonaliyev <mehmonaliyevijodbek@gmail.com>
2022-11-26 14:47:00 +0500

    'delet'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { useEffect, useState } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !==id);
+    setTours(newTours)
+  }
+
+const fetchTours = async () => {
+
+  try {
+    const responce = await fetch(url)
+    const tours = await responce.json();
+    setLoading(false)
+    setTours(tours)
+  } catch (error) {
+    setLoading(false)
+    console.log(error);
+  }
+}
+
+useEffect(() => {
+  fetchTours();
+}, [])
+
+  if(loading){
+    return(
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
     </div>
   );
 }
